032c7de2b7fc6f9cb0b8654adc3fa3addb44d44e
Brian Stone <brians@zillowgroup.com>
2017-05-30 17:18:57 -0400

    Fix reminder notifications.

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 (function () {
-    var port = chrome.runtime.connect(),
+    var port = chrome.runtime.connect();
         TYPE = {
             NEW_MAIL: 'New Mail',
             ALERTS: 'Alerts',
@@ -7,45 +7,10 @@
             IM_REQUESTS: 'IM requests'
         };
 
-    // "New Mail: 2", "Alerts: 0", "Reminders: 0", "IM requests:0"
-    function getNotificationType(title) {
-        if (title.indexOf(TYPE.NEW_MAIL) >= 0) {
-            return TYPE.NEW_MAIL;
-        } else if (title.indexOf(TYPE.ALERTS) >= 0) {
-            return TYPE.ALERTS;
-        } else if (title.indexOf(TYPE.REMINDERS) >= 0) {
-            return TYPE.REMINDERS;
-        } else if (title.indexOf(TYPE.IM_REQUESTS) >= 0) {
-            return TYPE.IM_REQUESTS;
-        }
-        port.postMessage({
-            error: 'Unknown notification type: ' + title
-        });
-        return null;
-    }
-
-    // "New Mail: 2", "Alerts: 0", "Reminders: 0", "IM requests:0"
-    function getNotificationNumber(title) {
-        return parseInt(title.match(/\d+/)[0], 10);
-    }
-
     function getNotifications() {
         var msg = {},
-            notifications = document.querySelectorAll('.o365cs-notifications-notificationEntryButton');
-        notifications = Array.prototype.slice.call(notifications);
-        notifications.forEach(function (notification) {
-            var title = notification.getAttribute('title'),
-                type = getNotificationType(title);
-
-            // Mail notifications are handled separately
-            if (type !== TYPE.NEW_MAIL) {
-                msg[type] = 0;
-                // Make sure notification is visible (up-to-date)
-                if (notification.clientHeight) {
-                    msg[type] = getNotificationNumber(title);
-                }
-            }
-        });
+            notifications = document.querySelectorAll('.o365cs-notifications-reminders-container');
+        msg[TYPE.REMINDERS] = notifications.length;
 
         // Post message back to plugin for new message display
         port.postMessage(msg);
